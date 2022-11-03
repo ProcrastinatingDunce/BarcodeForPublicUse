7506453d0277d5eb2c096f80d9e8d65b47609722
CroxxN <saradgaihre19@gmail.com>
2022-11-03 09:55:32 +0545

    Update working flask server

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print("Hello BarCode")
 
 from flask import Flask, render_template, request
 
-app = Flask("app")
+app = Flask(__name__)
 
 @app.route("/")
 def index():
@@ -11,10 +11,10 @@ def index():
 @app.route("/submit", methods=["GET", "POST"])
 def submit():
   if request.method == "GET":
-    text = request.args["text"]
-    return text
+    return "<h1> _id = 123 <br/> Name = Soap <br/> Price = 1200 </h1>"
   elif request.method == "POST":
-    text = request.form["text"]
+    text = request.get_json()
     return text
 
-app.run(host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080)
